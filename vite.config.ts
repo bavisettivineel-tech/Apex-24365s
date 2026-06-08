731e8ac0ef9925372ea72e2664ab3ed45bb29134
Vineel Bavisetti <bavisettivineel@gmail.com>
2026-06-08 09:20:32 +0530

    Fix Vercel 404 by installing nitro and importing relative path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from "./node_modules/nitro/dist/vite.mjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tailwindcss(),
-    tanstackStart({
-      server: {
-        preset: "vercel",
-      },
+    tanstackStart(),
+    nitro({
+      preset: "vercel",
     }),
     react(),
   ],
